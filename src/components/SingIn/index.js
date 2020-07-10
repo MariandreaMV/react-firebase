297165d3422e9b03321abfe696b828592aeef643
Mariandrea Maldonado <mariandrea.mv15@gmail.com>
2020-07-10 12:55:07 -0400

    adding react router and ready as a boilerplate without css rules

--- src/components/SingIn/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SingIn = () => (
-  <div>
-    <h1>SingIn</h1>
-  </div>
-);
-
-export default SingIn;
--- src/components/SingUp/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SingUp = () => (
-  <div>
-    <h1>SingUp</h1>
-  </div>
-);
-
-export default SingUp;
